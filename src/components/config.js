f11b70064962ede33e0faffe6f98c96361ab944b
Aneudy Adames <a4aneudy@gmail.com>
2020-11-30 23:42:40 -0500

    added demoaccount email and password to be env variables

---
+++ src/components/config.js
@@ -0,0 +1,8 @@
+const config = {
+  API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: 'https://stark-coast-96634.herokuapp.com/api',
+  DEMO_ACCOUNT_EMAIL: process.env.REACT_APP_DEMO_ACCOUNT_EMAIL,
+  DEMO_ACCOUNT_PASSWORD: process.env.REACT_APP_DEMO_ACCOUNT_PASSWORD,
+};
+
+export default config;
