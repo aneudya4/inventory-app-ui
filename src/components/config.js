4c68521471ef190dcd61e17077cb4a570e1745c0
Aneudy Adames <a4aneudy@gmail.com>
2020-11-30 23:36:41 -0500

    remove config file from git

--- src/components/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: 'https://stark-coast-96634.herokuapp.com/api',
-  DEMO_ACCOUNT_EMAIL: 'demoaccount@gmail.com',
-  DEMO_ACCOUNT_PASSWORD: '123456DemoAccount!',
-};
-
-export default config;
